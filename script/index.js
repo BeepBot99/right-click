589c2fce7c7c9696464b5ffa58032286010cd201
dluxy <74377508+dluxy@users.noreply.github.com>
2020-11-12 13:39:33 -0600

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,9 +1,21 @@
 class rightClickMenu {
   constructor(elem) {
     this.consoleDebug = true;
-    this.contextMenu = {
-     
-    };
+    this.contextMenu = [
+      {
+        type:"url",
+        value:"https://example.com",
+        text:"URL"
+      },
+      {
+        type:"javascript",
+        value:"alert('Hello, World!')",
+        text:"Javascript"
+      },
+      {
+        type:"hr"
+      }
+    ];
   }
  open() {
   }
